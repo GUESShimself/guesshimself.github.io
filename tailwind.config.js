90b83a3cedd00b15733c8821e9d64ee9b911d1f4
Eric Guess <411941+GUESShimself@users.noreply.github.com>
2025-05-13 18:09:01 -0400

    attempting to add tailwind back

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
+    darkMode: 'data-theme',
     content: ["./**/*.{html,js}"],
     theme: {
         extend: {},
